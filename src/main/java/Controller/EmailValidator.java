1ba542fd151ceed2c235000c60b48f3632f2532e
costas <costas@DESKTOP-GB1ASLH>
2016-09-21 20:13:13 +0300

    Pattern Form samples

--- src/main/java/Controller/EmailValidator.java
+++ src/main/java/Controller/EmailValidator.java
@@ -20,13 +20,13 @@ import javax.faces.validator.ValidatorException;
  *
  * @author costas
  */
-@FacesValidator("contro.EmailValidator")
+@FacesValidator("controller.EmailValidator")
 public class EmailValidator implements Validator{
 
-	private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-]+(\\." +
-			"[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*" +
-			"(\\.[A-Za-z]{2,})$";
-
+	private static final String EMAIL_PATTERN = "^([_ΑΒΓΔΕΖΗΘ0123456789]+)*$" ;
+        //private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-]+(\\." +
+	//		"[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*" +
+	//		"(\\.[A-Za-z]{2,})$";
 	private Pattern pattern;
 	private Matcher matcher;
 
